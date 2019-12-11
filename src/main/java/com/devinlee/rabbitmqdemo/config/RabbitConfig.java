bf86d36239aea16aac33e3f6de30cc05ffeec185
devinlee <devinlee24@foxmail.com>
2019-12-11 12:11:34 +0800

    RabbitMQ初始化配置

---
+++ src/main/java/com/devinlee/rabbitmqdemo/config/RabbitConfig.java
@@ -0,0 +1,81 @@
+package com.devinlee.rabbitmqdemo.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * RabitMQ初始化配置
+ * @author Devin Lee
+ * @date 20191211
+ */
+@Configuration
+public class RabbitConfig {
+
+    /**
+     * RabitMQ支持简单模式、工作队列模式、订阅/发布模式（fanout）、路由模式（direct）、主题模式（topic）
+     * @return
+     */
+    //==============发布/订阅模式==============
+
+    //队列A
+    @Bean()
+    public Queue queueA() {
+        return new Queue("fanoutQueueA");
+    }
+
+    //队列B
+    @Bean()
+    public Queue queueB() {
+        return new Queue("fanoutQueueB");
+    }
+
+    //发布订阅模式下的 fanout 交换机
+    @Bean
+    public FanoutExchange fanoutExchange() {
+        return new FanoutExchange("fanoutExchange");
+    }
+
+    // 把 队列A 和 fanout交换机 绑定在一起
+    @Bean
+    public Binding bindingWithQueueA() {
+        return BindingBuilder.bind(queueA()).to(fanoutExchange());
+    }
+
+    // 把 队列B 和 fanout交换机 绑定在一起
+    @Bean
+    public Binding bindingWithQueueB() {
+        return BindingBuilder.bind(queueB()).to(fanoutExchange());
+    }
+
+    //==============路由模式==============
+    //队列C
+    @Bean()
+    public Queue queueC() {
+        return new Queue("directQueueC");
+    }
+
+    //队列D
+    @Bean()
+    public Queue queueD() {
+        return new Queue("directQueueD");
+    }
+
+    //发布订阅模式下的 direct 交换机
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange("directExchange");
+    }
+
+    // 把 队列C 和 direct交换机 绑定在一起
+    @Bean
+    public Binding bindingWithQueueC() {
+        return BindingBuilder.bind(queueC()).to(directExchange()).with("directRoutingkey");
+    }
+
+    // 把 队列D 和 direct交换机 绑定在一起
+    @Bean
+    public Binding bindingWithQueueD() {
+        return BindingBuilder.bind(queueD()).to(directExchange()).with("directRoutingkey");
+    }
+}
